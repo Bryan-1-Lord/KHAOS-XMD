181f061396feaa7124f63d2e96adbebc792baf4d
Bryanlover1 <165657669+Bryanlover1@users.noreply.github.com>
2025-05-17 06:50:39 +0000

    Update menu.js

--- Bryan/menu.js
+++ Bryan/menu.js
@@ -10,7 +10,7 @@ const adams = require(__dirname + "/../config");
 
 async function fetchMENUUrl() {
   try {
-    const response = await axios.get(adams.BWM_XMD);
+    const response = await axios.get(adams.KHAOS_XMD);
     const $ = cheerio.load(response.data);
 
     const targetElement = $('a:contains("MENU")');
